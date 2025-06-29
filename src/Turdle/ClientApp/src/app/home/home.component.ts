86e207e92ff72376f975bd265557cfbaf6b91410
David Williams <david+@moneyboxapp.com>
2025-06-29 22:11:31 +0100

    Enlarge home page controls

--- src/Turdle/ClientApp/src/app/home/home.component.ts
+++ src/Turdle/ClientApp/src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { HomeService } from '../services/home.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
